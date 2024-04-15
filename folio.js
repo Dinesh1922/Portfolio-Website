479c09b132a3681b3a085b9754059907fe9df906
unknown <dineshvicky98@gmail.com>
2024-04-15 23:56:20 +0530

    updated more about me

--- folio.js
+++ folio.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Web Developer","Artist", "Influencer"],
+    strings: ["Web Developer", "Artist", "Influencer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
